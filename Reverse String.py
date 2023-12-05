ed712753080638e17c212591d45e04f749205dbf
Erica Markey <markeye@tcd.ie>
2023-12-05 20:07:58 +1100

    reverse string

---
+++ Reverse String.py
@@ -0,0 +1,7 @@
+#Code to take an input and reverse the string
+
+
+if __name__ == '__main__':
+    original = input('Please enter word to be reversed:')
+    reversed = original[::-1]
+    print(reversed)
